59349c861e2cf4421fb0d1a5fed7d986ea06130d
OlaMushroom <ola.mushroom@gmail.com>
2021-10-30 19:29:14 +0700

    Add files via upload

---
+++ code/C++/Algorithm design/9d.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int bai9d (int n)
+{
+    int i = 0;
+    int x = 0;
+    int y = n % 10;
+    while (n != 0)
+    {
+        x = n % 10;
+        if ((abs(x - y) > i))
+        {
+            i = abs(x - y);
+        }
+        y = x;
+        n /= 10;
+    }
+    return i;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a = bai9d(n);
+    cout<<a;
+    return 0;
+}
