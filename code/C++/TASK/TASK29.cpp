26af5d953810c26bf58d02efbe729e7ee6f7232a
Nyanto <ola.mushroom@gmail.com>
2021-12-21 22:38:44 +0700

    Add files via upload

---
+++ code/C++/TASK/TASK29.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int baitap (int n)
+{
+    int i = 0;
+    int S = 9;
+    while (n != 0)
+    {
+        i = n % 10;
+        if (i != 0 && i < S)
+        {
+            S = i;
+        }
+        n /= 10;
+    }
+    return S;
+}
+
+int main()
+{
+    freopen("TASK29.INP","r",stdin);
+    freopen("TASK29.OUT","w",stdout);
+    int n;
+    cin>>n;
+    int a = baitap(n);
+    cout<<a;
+    return 0;
+}
