e2d1c4220d387b2b9365db0093d9f2f0cc56ee2f
Nyanto <ola.mushroom@gmail.com>
2022-03-19 21:54:18 +0700

    Add files via upload

---
+++ code/C++/MANG/MANG01.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+#define maxN 1000000
+int a[maxN], n, x;
+
+void input()
+{
+    cin>>n>>x;
+    for (int i = 0; i < n; ++i)
+    {
+        cin>>a[i];
+    }
+}
+
+void factor()
+{
+    int cnt = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        if (a[i] != 0)
+            if (x % a[i] == 0)
+            {
+                cnt++;
+            }
+    }
+    cout<<cnt;
+}
+
+int main()
+{
+    freopen("MANG01.INP","r",stdin);
+    freopen("MANG01.OUT","w",stdout);
+    input();factor();
+    return 0;
+}
