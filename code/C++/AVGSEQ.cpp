2a4a56c8ac6f56e6c1f0d65f4ff790d279391088
Nyanto <ola.mushroom@gmail.com>
2022-04-10 20:50:52 +0700

    Add files via upload

---
+++ code/C++/AVGSEQ.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+vector<long long> a;
+vector<long long> b;
+int n;
+
+void input ()
+{
+    cin>>n;
+    a.resize(n);
+    b.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin>>b[i];
+        b[i] *= i + 1;
+    }
+
+}
+
+void output ()
+{
+    a[0] = b[0]; cout<<a[0]<<" ";
+    for (int i = 1; i < n; ++i)
+    {
+        a[i] = b[i] - b[i - 1];
+        cout<<a[i]<<" ";
+    }
+}
+
+int main()
+{
+    freopen("AVGSEQ.INP","r",stdin);
+    freopen("AVGSEQ.OUT","w",stdout);
+    input();output();
+    return 0;
+}
